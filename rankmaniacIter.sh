a7d4418ef6405edfe03cfbf4345dc85c821572b3
Rafael Fueyo-Gomez <rfueyogo@caltech.edu>
2019-02-10 16:58:41 -0800

    got an bash script that runs 5 iterations.

--- rankmaniacIter.sh
+++ rankmaniacIter.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
+cd data/
 
 python pagerank_map.py < input.txt | sort | python pagerank_reduce.py |
+python process_map.py | sort | python process_reduce.py |
 
-for i in `seq 1 10`;
-do
-    python pagerank_map.py | sort | python pagerank_reduce.py |
-    python process_map.py | sort | python process_reduce.py
-done
+python pagerank_map.py | sort | python pagerank_reduce.py |
+python process_map.py | sort | python process_reduce.py |
 
+python pagerank_map.py | sort | python pagerank_reduce.py |
+python process_map.py | sort | python process_reduce.py |
+
+python pagerank_map.py | sort | python pagerank_reduce.py |
+python process_map.py | sort | python process_reduce.py |
+
+python pagerank_map.py | sort | python pagerank_reduce.py |
 python process_map.py | sort | python process_reduce.py > output.txt
