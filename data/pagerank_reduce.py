6763c6aaff0d73fed1b9027818f29d67d1597273
Rafael Fueyo-Gomez <rfueyogo@caltech.edu>
2019-02-11 22:13:58 -0800

    pagerank reduce second try, deals with multiple key-value pairs

--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -7,9 +7,39 @@ alpha = 0.85
 adjacency_row = []
 columnSum = 0
 
+previousNode = ''
+firstIter = True
+
 for line in sys.stdin:
     # split string input
     node_id, value = line.split("\t")
+
+    # first iteration, set the node id
+    if firstIter:
+        previousNode = node_id
+
+    # if we just saw a new node, we gotta print what we have
+    elif previousNode != node_id:
+        # once done processing, multiply by alpha
+        columnSum = alpha * columnSum
+
+        # set as new rank (temporarily)
+        adjacency_row[0] = columnSum
+
+        # create the output string
+        output = 'NodeId:' + previousNode + '\t' + str(adjacency_row) + '\n'
+
+        # emit the row
+        sys.stdout.write(output)
+
+        # clear the values
+        adjacency_row = []
+        columnSum = 0
+        previousNode = node_id
+
+
+
+    firstIter = False
 
     # change value into a variable from a string
     value = ast.literal_eval(value)
@@ -22,7 +52,6 @@ for line in sys.stdin:
     else:
         columnSum += float(value)
 
-
 # once done processing, multiply by alpha
 columnSum = alpha * columnSum
 
@@ -30,7 +59,8 @@ columnSum = alpha * columnSum
 adjacency_row[0] = columnSum
 
 # create the output string
-output = node_id + '\t' + str(adjacency_row)
+output = 'NodeId:' + node_id + '\t' + str(adjacency_row) + '\n'
 
 # emit the row
 sys.stdout.write(output)
+
