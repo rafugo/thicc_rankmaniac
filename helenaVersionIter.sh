b0291d8757b5cddc5a1e08f096823e53f9152376
helenashield <hshield@caltech.edu>
2019-02-12 21:53:54 -0800

    fixed process reduce parsing

--- helenaVersionIter.sh
+++ helenaVersionIter.sh
@@ -5,23 +5,23 @@ cd data/
 python pagerank_map.py < input.txt | sort -k1,1 | python pagerank_reduce.py |
 python process_map.py | sort -k1,1 | python process_reduce.py |
 
-python pagerank_map.py | sort -k1.1 | python pagerank_reduce.py |
-python process_map.py | sort -k1.1 | python process_reduce.py |
+python pagerank_map.py | sort -k1,1 | python pagerank_reduce.py |
+python process_map.py | sort -k1,1 | python process_reduce.py |
 
-python pagerank_map.py | sort -k1.1 | python pagerank_reduce.py |
-python process_map.py | sort | python process_reduce.py |
+python pagerank_map.py | sort -k1,1 | python pagerank_reduce.py |
+python process_map.py | sort -k1,1| python process_reduce.py |
 
-python pagerank_map.py | sort -k1.1 | python pagerank_reduce.py |
-python process_map.py | sort -k1.1 | python process_reduce.py |
+python pagerank_map.py | sort -k1,1 | python pagerank_reduce.py |
+python process_map.py | sort -k1,1 | python process_reduce.py |
 
-python pagerank_map.py | sort -k1.1 | python pagerank_reduce.py |
-python process_map.py | sort -k1.1 | python process_reduce.py |
+python pagerank_map.py | sort -k1,1 | python pagerank_reduce.py |
+python process_map.py | sort -k1,1 | python process_reduce.py |
 
-python pagerank_map.py | sort -k1.1 | python pagerank_reduce.py |
-python process_map.py | sort -k1.1 | python process_reduce.py |
+python pagerank_map.py | sort -k1,1 | python pagerank_reduce.py |
+python process_map.py | sort -k1,1 | python process_reduce.py |
 
-python pagerank_map.py | sort -k1.1 | python pagerank_reduce.py |
-python process_map.py | sort -k1.1 | python process_reduce.py |
+python pagerank_map.py | sort -k1,1 | python pagerank_reduce.py |
+python process_map.py | sort -k1,1 | python process_reduce.py |
 
-python pagerank_map.py | sort -k1.1 | python pagerank_reduce.py |
-python process_map.py | sort -k1.1 | python process_reduce.py > output.txt
+python pagerank_map.py | sort -k1,1 | python pagerank_reduce.py |
+python process_map.py | sort -k1,1 | python process_reduce.py > output.txt
